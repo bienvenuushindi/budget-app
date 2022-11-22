bf0101440833e199340fb9b6b3ed45b15f4630f6
usbbush <jeanbienvenusb@gmail.com>
2022-11-22 14:30:32 +0200

    Authorize all actions in a RESTful style resource controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,4 +1,5 @@
 class ExpensesController < ApplicationController
+  load_and_authorize_resource
   before_action :set_expense, only: %i[ show edit update destroy ]
 
   # GET /expenses or /expenses.json
