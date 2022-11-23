5e5500ded9fff45decc72a3775b04d5fd556cee3
usbbush <jeanbienvenusb@gmail.com>
2022-11-23 18:11:27 +0200

    Add constraint to association

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,6 @@
 class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
-  has_and_belongs_to_many :categories
+  has_and_belongs_to_many :categories, dependent: :destroy
   validates :name, presence: true,  length: { minimum: 3 }
   validates :amount,presence: true, numericality: {greater_than_or_equal_to: 0}
 
@@ -10,10 +10,14 @@ class Expense < ApplicationRecord
   private
 
   def update_category_total_amount
-    categories.increment!(:total_amount, amount)
+    categories.each do |category|
+      category.increment!(:total_amount, amount)
+    end
   end
 
   def reduce_category_total_amount
-    categories.decrement!(:total_amount, amount)
+    categories.each do |category|
+      category.decrement!(:total_amount, amount)
+    end
   end
 end
