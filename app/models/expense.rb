145e9859d7d7806bb04ffdc1b2a847e491c3c102
usbbush <jeanbienvenusb@gmail.com>
2022-11-23 10:40:22 +0200

    Implement method to increment and decrement categories total_amount

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,4 +3,17 @@ class Expense < ApplicationRecord
   has_and_belongs_to_many :categories
   validates :name, presence: true,  length: { minimum: 3 }
   validates :amount,presence: true, numericality: {greater_than_or_equal_to: 0}
+
+  after_save  :update_category_total_amount
+  before_destroy :reduce_category_total_amount
+
+  private
+
+  def update_category_total_amount
+    categories.increment!(:total_amount, amount)
+  end
+
+  def reduce_category_total_amount
+    categories.decrement!(:total_amount, amount)
+  end
 end
