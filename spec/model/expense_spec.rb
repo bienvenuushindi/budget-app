4cdb5740a50af55a9bab9e4554c07917a244aba6
usbbush <jeanbienvenusb@gmail.com>
2022-11-23 17:16:37 +0200

    Update expense sepc

--- spec/model/expense_spec.rb
+++ spec/model/expense_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 RSpec.describe 'Expense', type: :model do
-  user= User.new(name: 'Ubs', email: 'jeanbienvenusb@gmail.com', password: 'my@password')
-  subject{Expense.new(author: user, name: 'Electricity', amount: 10)}
+  # user=User.new(name: 'Ubs', email: 'jeanbienvenusb@gmail.com', password: 'my@password', password_confirmation: "my@password")
+  subject{Expense.new(user_id: 1, name: 'Electricity', amount: 10)}
+  before { subject.save }
+
   it 'name should be present' do
     subject.name = nil
     expect(subject).to_not be_valid
@@ -15,9 +17,7 @@ RSpec.describe 'Expense', type: :model do
     expect(subject).to_not be_valid
   end
   it 'amount should be non-negative integer' do
+    subject.amount = -1
     expect(subject).to_not be_valid
-  end
-  it 'has and belongs to many categories' do
-    should have_and_belong_to_many :categories
   end
 end
