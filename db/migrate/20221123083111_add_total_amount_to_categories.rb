3d3efdc7e93e981ef0dee11a9b5cece1a28f5414
usbbush <jeanbienvenusb@gmail.com>
2022-11-23 10:43:05 +0200

    Add default value to column

--- db/migrate/20221123083111_add_total_amount_to_categories.rb
+++ db/migrate/20221123083111_add_total_amount_to_categories.rb
@@ -1,5 +1,5 @@
 class AddTotalAmountToCategories < ActiveRecord::Migration[7.0]
   def change
-    add_column :categories, :total_amount, :integer
+    add_column :categories, :total_amount, :integer, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_094758) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_083111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_094758) do
     t.string "icon"
     t.datetime "created_at"
     t.bigint "user_id", null: false
+    t.integer "total_amount", default: 0
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
