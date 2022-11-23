d893c2e9c08719a808427c790514e147a56644fa
usbbush <jeanbienvenusb@gmail.com>
2022-11-23 10:37:53 +0200

    Make expenses subroute of categories

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ Rails.application.routes.draw do
 
   root 'home#index'
   resources :users
-  resources :expenses
-  resources :categories
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :categories do
+    resources :expenses
+  end
 end
