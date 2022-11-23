0810c31ceca155dcc6dbd14e99050572e7080d7d
usbbush <jeanbienvenusb@gmail.com>
2022-11-23 17:17:20 +0200

    redirect to splash screen after sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(_resource_or_scope)
-    new_user_session_path
+    welcome_path
   end
 
   def after_sign_in_path_for(_resource_or_scope)
