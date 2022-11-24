31979d9897b649d39345ab27a598dce9eb49f9c4
usbbush <jeanbienvenusb@gmail.com>
2022-11-24 16:25:04 +0200

    Implement forms submission

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+document.addEventListener('click', (e) => {
+    console.log(e.target.id)
+    if (e.target.id === 'process-form') {
+        const Form = document.getElementById('new_user') || document.getElementById('new-category') || document.getElementById('new-transaction')
+        Form.submit()
+    }
+})
