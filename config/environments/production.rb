3e7193821b853cd4dc0534571d39eaf51cb696cc
usbbush <jeanbienvenusb@gmail.com>
2022-12-29 00:36:01 +0200

    Update configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "budget-app-production-e96d.up.railway.app"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -17,6 +17,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
