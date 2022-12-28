2771a53dbc030e430b82b5ba11a3a835d610f2c1
usbbush <jeanbienvenusb@gmail.com>
2022-12-29 00:45:10 +0200

    Update configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "budget-app-production-e96d.up.railway.app"
+  config.hosts << "budgetapp.up.railway.app"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
